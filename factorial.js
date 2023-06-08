4f085f8bf092f687a349b60514e9f57265bffa95
priyansu04 <priyansu0404@gmail.com>
2023-06-08 15:12:34 +0530

    first commit

---
+++ factorial.js
@@ -0,0 +1,11 @@
+function  main (n){
+    let f = 1
+    for (let i=1;i<=n;i++){
+        f= f*i
+    }
+    console.log(f)
+}
+
+main(5)
+main(7)
+main(10)
---
+++ sum.js
@@ -0,0 +1,6 @@
+function sum(a,b)
+{
+    const s= a+b
+    console.log(s)
+}
+sum(100,50)
